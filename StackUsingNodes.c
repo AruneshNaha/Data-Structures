c370e559a446441ae162ec4605ebb32a18ea8449
AruneshNaha <37961333+AruneshNaha@users.noreply.github.com>
2018-10-23 15:23:46 +0530

    Update StackUsingNodes.c

--- StackUsingNodes.c
+++ StackUsingNodes.c
@@ -1,48 +1,81 @@
 #include<stdio.h>
+#include<conio.h>
 #include<stdlib.h>
+
 struct stack{
 int data;
 struct stack *link;
 }*top=NULL;
 
+typedef struct stack stk;
+
 void push()
 {
-stk *tmp;
-tmp=(stk*)malloc(sizeof(stk));
-printf("Enter the data:");
-scanf("%d",&tmp->data);
-if(top==NULL)
-  tmp->link=NULL;
-else
-  tmp->link=top;
-top=tmp;
+    stk *tmp;
+    tmp=(stk*)malloc(sizeof(stk));
+    printf("Enter the data:");
+    scanf("%d",&tmp->data);
+    if(top==NULL)
+        tmp->link=NULL;
+    else
+        tmp->link=top;
+    top=tmp;
+    printf("Insertion Success!\n");
 }
 
 void pop()
 {
-stk *tmp;
-if(top=NULL)
-  printf("stack Underflow!\n");
-else
-{
-  tmp=top;
-  printf("%d",tmp->data);
-  top=tmp->link;
-  free(tmp);
-  }
-  }
-  
+    stk *tmp;
+    if(top==NULL)
+    {
+        printf("Stack Underflow!!!\n");
+    }
+    else
+    {
+        tmp=top;
+        printf("Element returned=%d\n",tmp->data);
+        top=tmp->link;
+        free(tmp);
+    }
+}
+
 void display()
 {
-stk *tmp;
-if(top==NULL)
-  printf("Stack Underflow!!!\n");
-else
-  do
-  {
-    tmp=top;
-    printf("%d->",tmp->data);
-    tmp=tmp->link;
-    }while(tmp->link!=NULL);
-printf("%d->NULL",tmp->data);
+    stk *tmp;
+    if(top==NULL)
+        printf("Stack Underflow!!!\n");
+    else
+    {
+        tmp=top;
+        while(tmp->link!=NULL)
+        {
+            printf("%d->",tmp->data);
+            tmp=tmp->link;
+        }
+        printf("%d->NULL",tmp->data);
+    }
+}
+
+void main()
+{
+    int ch;
+    ch=1;
+    printf("1->push, 2->pop, 3->display, 4->exit\n");
+    while(ch!=4)
+    {
+        printf("Enter your choice:");
+        scanf("%d",&ch);
+        switch(ch)
+        {
+            case 1:push();
+            break;
+            case 2:pop();
+            break;
+            case 3:display();
+            break;
+            case 4:exit(0);
+            break;
+            default:printf("Wrong choice!\n");
+        }
+    }
 }
