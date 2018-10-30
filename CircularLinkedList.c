3ebc06840439382ea637bdfe4e390f16beb4dd30
AruneshNaha <37961333+AruneshNaha@users.noreply.github.com>
2018-10-30 23:15:21 +0530

    Circular Queue program
    This program is'nt perfect, needs changes!!!

---
+++ CircularLinkedList.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+    int data;
+    struct node *prev,*next;
+}*head=NULL;
+
+typedef struct node Node;
+
+void create()
+{
+    Node *tmp1,*tmp2;
+    int ch=1;
+    while(ch!=0)
+    {
+        tmp1=(Node*)malloc(sizeof(Node));
+        printf("Enter node data:");
+        scanf("%d",&tmp1->data);
+        if(head==NULL)
+        {
+            tmp1->next=NULL;
+            tmp1->prev=NULL;
+            tmp2=tmp1;
+        }
+        else
+        {
+            tmp1->next=NULL;
+            tmp2->next=tmp1;
+            tmp1->prev=tmp2;
+            tmp2=tmp1;
+        }
+        printf("Any more nodes? [1/0] :");
+        scanf("%d",&ch);
+    }
+}
+
+void display()
+{
+    Node *tmp;
+    tmp=head;
+    while(tmp->next!=NULL)
+    {
+        printf("%d->",tmp->data);
+        tmp=tmp->next;
+    }
+    printf("%d->NULL",tmp->data);
+}
+
+void main()
+{
+    create();
+    display();
+}
